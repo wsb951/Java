93c870b377d1d32da9ff9490e1fdfbcb0da63c6a
wsb951 <840580592@qq.com>
2023-10-18 16:04:28 +0800

    test

---
+++ src/MyThreadPool.java
@@ -0,0 +1,7 @@
+import java.util.concurrent.*;
+
+public class MyThreadPool {
+    public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
